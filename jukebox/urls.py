f4118d140f9a584ebeec4691439e33984f7e7702
Kat Stevens <katstevens@users.noreply.github.com>
2016-02-16 22:55:29 +0000

    Added myblurbs view, url, template

--- jukebox/urls.py
+++ jukebox/urls.py
@@ -19,12 +19,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from blurber.views import weekly_schedule, write_review
+from writers.views import my_blurbs
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^schedule/', weekly_schedule, name='weekly_schedule'),
     url(r'^song/(?P<song_id>\d+)$', write_review, name='write_review'),
     url(r'^song/(?P<song_id>\d+)/html/$', write_review, {'use_html': True}, name='write_review_html'),
+
+    url(r'^myblurbs/$', my_blurbs, name='my_blurbs'),
+    url(r'^myblurbs/(?P<status>\w+)/$', my_blurbs, name='my_blurbs_by_status'),
+    url(r'^myblurbs/(?P<year>\d+)/$', my_blurbs, name='my_blurbs_by_year'),
+    url(r'^myblurbs/(?P<year>\d+)/(?P<status>\w+)/$', my_blurbs, name='my_blurbs_by_year_and_status'),
 ]
 
 if settings.DEBUG:
--- writers/views.py
+++ writers/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from blurber.models import Review
+
+
+CURRENT_YEAR = datetime.now().year
+
+
+def my_blurbs(request, status=None, year=None):
+
+    reviews = Review.objects.filter(writer=request.user).exclude(status='removed')
+
+    filter_year = year if year else CURRENT_YEAR
+    reviews = reviews.filter(create_date__year=filter_year)
+
+    if status == 'open':
+        reviews = reviews.filter(song__status__in=['open', 'closed'])
+    elif status == 'published':
+        reviews = reviews.filter(song__status='published')
+
+    return render(
+        request,
+        'my_blurbs.html',
+        {
+            'reviews': reviews,
+            'status': status,
+            'year': year
+        }
+    )
