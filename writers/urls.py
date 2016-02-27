033fa68e447626f768a402c48a5d7ebad17a814c
Kat Stevens <katstevens@users.noreply.github.com>
2016-02-27 18:26:59 +0000

    Editors can view all blurbs by a writer

--- writers/urls.py
+++ writers/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from writers.views import my_blurbs
+from writers.views import my_blurbs, writer_blurbs
 
 urlpatterns = [
     # The below are included under the /myblurbs url space
@@ -8,4 +8,11 @@ urlpatterns = [
     url(r'^(?P<status>\w+)/$', my_blurbs, name='my_blurbs_by_status'),
     url(r'^(?P<year>\d+)/$', my_blurbs, name='my_blurbs_by_year'),
     url(r'^(?P<year>\d+)/(?P<status>\w+)/$', my_blurbs, name='my_blurbs_by_year_and_status'),
+
+    # Editors only
+    url(r'^writer/(?P<writer_id>\d+)/$', writer_blurbs, name='writer_blurbs'),
+    url(r'^writer/(?P<writer_id>\d+)/(?P<status>\w+)/$', writer_blurbs, name='writer_blurbs_by_status'),
+    url(r'^writer/(?P<writer_id>\d+)/(?P<year>\d+)/$', writer_blurbs, name='writer_blurbs_by_year'),
+    url(r'^writer/(?P<writer_id>\d+)/(?P<year>\d+)/(?P<status>\w+)/$', writer_blurbs, name='writer_blurbs_by_year_and_status'),
+
 ]
--- writers/views.py
+++ writers/views.py
@@ -1,12 +1,16 @@
 from datetime import datetime
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
 from blurber.models import Review
+from writers.models import Writer
 
 
 CURRENT_YEAR = datetime.now().year
 
 
+@login_required()
 def my_blurbs(request, status=None, year=None):
 
     reviews = Review.objects.filter(writer=request.user).exclude(status='removed')
@@ -14,7 +18,7 @@ def my_blurbs(request, status=None, year=None):
     filter_year = year if year else CURRENT_YEAR
     reviews = reviews.filter(create_date__year=filter_year)
 
-    if status == 'open':
+    if status == 'saved':
         reviews = reviews.filter(song__status__in=['open', 'closed'])
     elif status == 'published':
         reviews = reviews.filter(song__status='published')
@@ -25,6 +29,37 @@ def my_blurbs(request, status=None, year=None):
         {
             'reviews': reviews,
             'status': status,
-            'year': year
+            'year': year,
+            'writer': request.user,
+            'editor_view': False
+        }
+    )
+
+
+@staff_member_required(login_url='login')
+def writer_blurbs(request, writer_id, status=None, year=None):
+    # An editor can view all the blurbs for a writer
+    # with links to edit it in the admin...
+    # TODO: restrict editors admin privileges to blurb only
+    writer = get_object_or_404(Writer, id=writer_id)
+    reviews = Review.objects.filter(writer=writer).exclude(status='removed')
+
+    filter_year = year if year else CURRENT_YEAR
+    reviews = reviews.filter(create_date__year=filter_year)
+
+    if status == 'saved':
+        reviews = reviews.filter(song__status__in=['open', 'closed'])
+    elif status == 'published':
+        reviews = reviews.filter(song__status='published')
+
+    return render(
+        request,
+        'my_blurbs.html',
+        {
+            'reviews': reviews,
+            'status': status,
+            'year': year,
+            'writer': writer,
+            'editor_view': True
         }
     )
