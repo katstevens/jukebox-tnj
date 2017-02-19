7e90b4ee28c80b9c4edc7fdbec5a5d7fd1fbc7d6
Kat Stevens <katstevens@users.noreply.github.com>
2017-02-19 11:10:40 +0000

    Additional links

--- writers/admin.py
+++ writers/admin.py
@@ -6,7 +6,7 @@ from writers.models import Writer
 class WriterAdmin(UserAdmin):
 
     # TODO: include extra fields e.g. bio_link
-    list_display = ['first_name', 'last_name', 'is_staff']
+    list_display = ['first_name', 'last_name', 'username', 'is_staff']
     list_filter = ['is_staff']
     ordering = ['last_name', 'first_name']
     search_fields = ['last_name', 'first_name']
