7c9b6d4cf79bf5a8507de3bd868a2eace1e2db2a
Kat <kat.stevens@gmail.com>
2018-12-28 14:01:30 +0000

    PublicPost model for cached published posts

--- tsj/admin.py
+++ tsj/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from tsj.models import PublicPost
 
 # Register your models here.
+admin.register(PublicPost)
--- tsj/models.py
+++ tsj/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from blurber.models import Song
 
-# Create your models here.
+
+class PublicPost(models.Model):
+    """
+    A 'cached' copy of the song and its reviews that is
+    visible on the public site.
+    This allows quicker searching until a proper search function can be built.
+    """
+    song = models.ForeignKey(Song)
+    html_content = models.TextField(help_text="Only edit this in an emergency!")
+    visible = models.BooleanField(default=True)
+    include_in_search_results = models.BooleanField(default=True)
+    published_on = models.DateField()
+
+    def __str__(self):
+        self.song.__str__()
